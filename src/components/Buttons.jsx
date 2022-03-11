4389b7154fd85662595aeaf24314573cae24a15d
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-03-11 04:57:00 +0100

    icons for buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,15 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBold,
+  faItalic,
+  faUnderline,
+  faAlignLeft,
+  faAlignCenter,
+  faAlignRight,
+  faAlignJustify,
+  faM,
+} from "@fortawesome/free-solid-svg-icons";
+import uppercase from "./../images/uppercase.svg";
+import lineThrough from "./../images/line-through.svg";
+import smallCaps from "./../images/small-caps.svg";
+
 const Buttons = ({ handleOption, viewStyle }) => {
   const options = [
-    "bold",
-    "italic",
-    "underline",
-    "line-through",
-    "uppercase",
-    "small-caps",
-    "left",
-    "center",
-    "right",
-    "justify",
+    { name: "bold", icon: faBold },
+    { name: "italic", icon: faItalic },
+    { name: "underline", icon: faUnderline },
+    { name: "line-through", icon: faM }, // todo
+    { name: "uppercase", icon: uppercase }, // todo
+    { name: "small-caps", icon: faM }, // todo
+    { name: "left", icon: faAlignLeft },
+    { name: "center", icon: faAlignCenter },
+    { name: "right", icon: faAlignRight },
+    { name: "justify", icon: faAlignJustify },
   ];
 
   return (
@@ -17,14 +32,28 @@ const Buttons = ({ handleOption, viewStyle }) => {
       {options.map((o, key) => (
         <button
           className={`option o-${o} ${
-            Object.values(viewStyle).indexOf(o) > -1 ? "active" : ""
+            Object.values(viewStyle).indexOf(o.name) > -1 ? "active" : ""
           }`}
           key={key}
           onClick={() => {
-            handleOption(o);
+            handleOption(o.name);
           }}
         >
-          {o.charAt(0)}
+          {o.name === "uppercase" ? (
+            <span>
+              <img src={uppercase} width={14} alt="uppercase" />
+            </span>
+          ) : o.name === "line-through" ? (
+            <span>
+              <img src={lineThrough} width={14} alt="line-through" />
+            </span>
+          ) : o.name === "small-caps" ? (
+            <span>
+              <img src={smallCaps} width={14} alt="small-caps" />
+            </span>
+          ) : (
+            <FontAwesomeIcon icon={o.icon} />
+          )}
         </button>
       ))}
     </>
